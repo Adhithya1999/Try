1e59b17947d8cba128402d5136423dc36cd7e91b
Adhithya1999 <anirudhadhithya20@gmail.com>
2022-06-21 13:38:02 +0530

    changed to my country

--- India.java
+++ India.java
@@ -2,7 +2,7 @@ public class India {
 
  public static void main(String[] args) {
   // TODO Auto-generated method stub
-    System.out.println("POPULAR");
+    System.out.println("My country");
 
  }
 
