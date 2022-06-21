0b4333527a972ef67891f1c62c6df162906c233d
Adhithya1999 <anirudhadhithya20@gmail.com>
2022-06-21 12:58:38 +0530

    changed the sentence

--- India.java
+++ India.java
@@ -2,7 +2,7 @@ public class India {
 
  public static void main(String[] args) {
   // TODO Auto-generated method stub
-    System.out.println("Hello INDIA");
+    System.out.println("POPULAR");
 
  }
 
